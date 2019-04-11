d5c024df6af1ac884a27ca270ff6a94a26ba3d62
Oscar <oscar.vanderhorstvalette@wmich.edu>
2019-04-10 22:04:15 -0400

    Made it a restful api

--- application.py
+++ application.py
@@ -1,14 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello':'world'}
 
-api.add_resource(HelloWorld, '/')
+def get_route(city1, city2):
+    if (city1 == "A" and city2 == "B"):
+        return {
+            "distance" : 1000,
+            "car" : { "price" : 30, "speed" : 40},
+            "bus" : { "price" : 60, "speed" : 50},
+            "train" : { "price" : 70, "speed" : 140},
+            "airplane" : {"price" : 210, "speed" : 520}
+        }
+    elif (city1 == "B" and city2 == "C"):
+         return {
+            "distance" : 2500,
+            "car" : { "price" : 30, "speed" : 40},
+            "bus" : { "price" : 60, "speed" : 50},
+            "train" : { "price" : 70, "speed" : 140},
+            "airplane" : {"price" : 210, "speed" : 520}
+        }
+    elif (city1 == "D" and city2 == "A"):
+         return {
+            "distance" : 200,
+            "car" : { "price" : 30, "speed" : 40},
+            "bus" : { "price" : 60, "speed" : 50},
+            "train" : { "price" : 70, "speed" : 140},
+        }
+
+class GetInfo(Resource):
+    def get(self):
+        return { "cities" : ["A", "B"] }
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        city1 = json_data['city1']
+        city2 = json_data['city2']
+
+        return jsonify(get_route(city1, city2))
+
+
+api.add_resource(GetInfo, '/api')
 
 if __name__ == "__main__":
-    app.run(debug=true)
+    app.run(debug=True)
